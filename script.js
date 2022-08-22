b62029325a6c1378a68f47466284a4edc5afe56d
Vignes Kumar Raj M <vigneskumar@piktorlabs.com>
2022-08-22 17:17:00 +0530

    22/8/2022 5:16PM

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function remove() {
+    document.getElementById("parent").classList.remove("parent1")
+    document.getElementById("parent").classList.remove("parent2")
+    document.getElementById("parent").classList.remove("parent3")
     document.getElementById("first").classList.remove("First1");
     document.getElementById("second").classList.remove("Second1");
     document.getElementById("third").classList.remove("Third1");
@@ -7,48 +10,54 @@ function remove() {
     document.getElementById("third").classList.remove("Third2");
 }
 
-const grid1 = document.querySelector(".grid1");
-animateCSSGrid.wrapGrid(grid1, {
+const first = document.querySelector(".first");
+animateCSSGrid.wrapGrid(first, {
     duration: 600
 });
 
-const grid2 = document.querySelector(".grid2");
-animateCSSGrid.wrapGrid(grid2, {
+const second = document.querySelector(".second");
+animateCSSGrid.wrapGrid(second, {
     duration: 600
 });
 
-const grid3 = document.querySelector(".grid3");
-animateCSSGrid.wrapGrid(grid3, {
+const third = document.querySelector(".third");
+animateCSSGrid.wrapGrid(third, {
     duration: 600
 });
 
 function a1() {
     remove();
+    document.getElementById("parent").classList.add("parent1");
     document.getElementById("first").classList.add("First1");
 }
 
 function a2() {
     remove();
+    document.getElementById("parent").classList.add("parent1");
     document.getElementById("first").classList.add("First2");
 }
 
 function a3() {
     remove();
+    document.getElementById("parent").classList.add("parent2");
     document.getElementById("second").classList.add("Second1");
 }
 
 function a4() {
     remove();
+    document.getElementById("parent").classList.add("parent2");
     document.getElementById("second").classList.add("Second2");
 }
 
 
 function a5() {
     remove();
+    document.getElementById("parent").classList.add("parent3");
     document.getElementById("third").classList.add("Third1");
 }
 
 function a6() {
     remove();
+    document.getElementById("parent").classList.add("parent3");
     document.getElementById("third").classList.add("Third2");
 }
